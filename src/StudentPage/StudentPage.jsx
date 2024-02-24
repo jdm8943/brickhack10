3afd34f26b9e8ceff1672584383e71d10e4a6dc2
Zak Kaufman <Zak3148@rit.edu>
2024-02-24 14:52:51 -0500

    started navbar updates

--- src/StudentPage/StudentPage.jsx
+++ src/StudentPage/StudentPage.jsx
@@ -9,18 +9,19 @@ class StudentPage extends Component {
 
     render = () => {
         return (
-            <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">Your Brand</Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="mr-auto">
-                        <Nav.Link href="#profile">Profile</Nav.Link>
-                        <Nav.Link href="#courses">Courses</Nav.Link>
-                        <Nav.Link href="#learn">Learn</Nav.Link>
-                        <Nav.Link href="#leaderboard">Leaderboard</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
+            <div id={"mainMenuNavBar"}>
+                <Navbar bg="light" expand="lg">
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="mr-auto">
+                            <Nav.Link href="#profile">Profile</Nav.Link>
+                            <Nav.Link href="#courses">Courses</Nav.Link>
+                            <Nav.Link href="#learn">Learn</Nav.Link>
+                            <Nav.Link href="#leaderboard">Leaderboard</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+            </div>
         )
     }
 }
