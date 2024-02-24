7c25010ab9c5c0686c101f918c41966abb4ae9f7
Zak Kaufman <Zak3148@rit.edu>
2024-02-24 14:29:18 -0500

    started updating

--- src/App.jsx
+++ src/App.jsx
@@ -2,46 +2,27 @@ import React, { Component } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
-import {auth, app, db} from './firebase.js';
+import { auth, app, db } from './firebase.js';
+import { Navbar } from 'react-bootstrap';
+import { StudentPage } from './StudentPage/StudentPage.jsx'; from;
 
 
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      count: 0,
-    };
+    this.state = {};
   }
 
   incrementCount = () => {
     this.setState({ count: this.state.count + 1 });
   };
 
-  render() {
+  render = () => {
     return (
-      <>
-        <div>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={this.incrementCount}>
-            count is {this.state.count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </>
+      <div>test
+        <StudentPage></StudentPage>
+      </div>
     );
   }
 }
