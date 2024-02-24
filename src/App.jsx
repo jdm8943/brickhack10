4dfbd6acb2c604c6cd3cb76c58aedf5f35d49c5e
jdm8943 <jdm8943@g.rit.edu>
2024-02-24 14:06:27 -0500

    move firebase out of app

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,9 @@ import React, { Component } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import {app} from './firebase.js';
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore, collection, getDocs, query } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDvsrgc09GqGD8y3a3H3G0xEdtx7vxF5Fg",
-  authDomain: "brickhack10.firebaseapp.com",
-  projectId: "brickhack10",
-  storageBucket: "brickhack10.appspot.com",
-  messagingSenderId: "1090128869892",
-  appId: "1:1090128869892:web:cc063de784b73b2842d351",
-  measurementId: "G-4J4WL8FHTK"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const db = getFirestore(app);
 
 class App extends Component {
   constructor(props) {
