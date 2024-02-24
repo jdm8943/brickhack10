d7f5d5b777e0b4ed7d91b7b4780cbaae9cdfe203
Zak Kaufman <Zak3148@rit.edu>
2024-02-24 16:17:55 -0500

    setup for spenc to start student profilePage

--- src/StudentProfilePage/StudentProfilePage.jsx
+++ src/StudentProfilePage/StudentProfilePage.jsx
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
+import { propTypes } from 'react-bootstrap/esm/Image';
+
 class StudentProfilePage extends Component {
-    defaultProps = {}
+    // TODO: change Later
+    defaultProps = {
+        userName: "spenc"
+    }
     state = {};
     render = () => {
         return <></>
     }
 }
+// TODO: uncomment this later once we have more setup
+// StudentProfilePage.propTypes = {
+//     userName: PropTypes.string.isRequired
+// };
 
 export default StudentProfilePage;
