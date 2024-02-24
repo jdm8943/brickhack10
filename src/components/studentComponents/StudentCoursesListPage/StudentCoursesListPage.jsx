7b4253c8bff96d0c71446cfc65fccaf0c3dbcf00
Spencer Kurtz <sjk7876@rit.edu>
2024-02-24 17:03:36 -0500

    setup

--- src/components/studentComponents/StudentCoursesListPage/StudentCoursesListPage.jsx
+++ src/components/studentComponents/StudentCoursesListPage/StudentCoursesListPage.jsx
@@ -10,23 +10,7 @@ class StudentCoursesPage extends Component {
 
     render = () => {
         return (
-            <>
-                <Row>
-                    <Col>
-                        <div bg="dark" data-bs-theme="dark">
-                            <h1>
-                                Welcome, {this.state.userName}
-                            </h1>
-                            <InputGroup>
-                                <Form.Control defaultValue={this.props.userName} onChange={e => this.handleInputChange(e)} />
-                                <Button variant="outline-dark" onClick={this.onSubmit}>
-                                    Change Display Name
-                                </Button>
-                            </InputGroup>
-                        </div>
-                    </Col>
-                </Row>
-            </>
+            
         )
     }
 }
