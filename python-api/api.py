47d2e956004967e407d9e8d91c6179f635cd9f6d
Spencer Kurtz <sjk7876@rit.edu>
2024-02-25 05:30:29 -0500

    api interacts with openai

--- python-api/api.py
+++ python-api/api.py
@@ -1,16 +1,17 @@
+import traceback
+
 import logging
 from fastapi import FastAPI
 import sqlite3
-
 from pydantic import BaseModel
-
+from aiCaller import callAI
+import os
 
 class command(BaseModel):
     cmd: str
-    correct_cmd: str | None = None
-
-con = sqlite3.connect("dbs/tutorial.db")
-cur = con.cursor()
+    correct_cmd: str
+    description: str
+    db: str
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -19,10 +20,20 @@ logger.addHandler(logging.StreamHandler())
 app = FastAPI()
 
 @app.post("/check_cmd")
-async def root(request: command ):    
-    res = cur.execute(request.cmd)
+async def root(request: command ):
+    con = sqlite3.connect(os.path.join("dbs", request.db + ".db"))
+    cur = con.cursor()
     
-    logger.info(request.cmd)
+    try:
+        cur.execute(request.cmd)
+        return {"message": "You did great"}
     
-    return {"message": res.fetchall()}
+    except Exception as e:
+        logger.info("SQL command failed! - " + request.cmd)
+    
+    returnMessage = callAI(request.cmd, request.correct_cmd, request.description, request.db)
+    
+    return {"message": returnMessage}
+    
+    
 
