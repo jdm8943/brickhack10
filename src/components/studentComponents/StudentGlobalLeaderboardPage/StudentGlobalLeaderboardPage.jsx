37cb9348b47c342f6e85278839280d9389956607
jdm8943 <jdm8943@g.rit.edu>
2024-02-25 05:04:15 -0500

    retrieve users for leaderboard

--- src/components/studentComponents/StudentGlobalLeaderboardPage/StudentGlobalLeaderboardPage.jsx
+++ src/components/studentComponents/StudentGlobalLeaderboardPage/StudentGlobalLeaderboardPage.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import { collection, getDocs, query } from '@firebase/firestore';
 
 class StudentGlobalLeaderboardPage extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            users: [],
+        }
+    }
+
+    componentDidMount = () => {
+        this.populateGlobalLeaderBoard()
     }
 
     populateGlobalLeaderBoard = () => {
-        
+        const usersQ = query(collection(this.props.firestoredb, "Users"))
+
+        getDocs(usersQ)
+            .then((usersSnapshot) => {
+                const uDocs = []
+                usersSnapshot.forEach((uDoc) => {
+                    uDocs.push({ ...uDoc.data(), id: uDoc.id });
+                })
+                return uDocs;
+            })
+            .then((users) => {
+                this.setState(
+                    { users: users },
+                    () => {console.log(users)}
+                )
+            }).catch((error) => {
+                console.log("Error getting users: ", error);
+            })
     }
 
     render(){
