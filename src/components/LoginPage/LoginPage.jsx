be327e9974a42168ecde0223352ac566d3cf56a9
Spencer Kurtz <sjk7876@rit.edu>
2024-02-25 11:19:51 -0500

    changed login to card

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import SignInButton from './SignInButton';
-import { Row, Col, ToggleButton } from 'react-bootstrap';
+import { Row, Col, ToggleButton, Card } from 'react-bootstrap';
 
 class LoginPage extends Component {
     constructor(props) {
@@ -19,29 +19,39 @@ class LoginPage extends Component {
     }
 
     render() {
-        return <div style={{textAlign:"center"}}>
-            <Row>
-                <Col>
-                    <h1 className="text-center">Welcome to SQueaL</h1>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <ToggleButton
-                        onClick={this.isInstructorClicked}
-                        style={{marginTop: "10px"}}
-                        >
-                        {this.state.isInstructor ? 'Signing in as Instructor' : 'Signing in as Student'}
-                    </ToggleButton>
-                </Col>
-            </Row>
-            <Row>
-                <Col>
-                    <SignInButton {...this.props} isInstructor={this.state.isInstructor} ></SignInButton>
-                </Col>
-            </Row>
-
-        </div>
+        return (
+            <div style={{textAlign:"center"}}>
+                {/* <Row>
+                    <Col>
+                        <h1 className="text-center">Welcome to SQueaL</h1>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col>
+                        <SignInButton {...this.props} isInstructor={this.state.isInstructor} ></SignInButton>
+                    </Col>
+                </Row> */}
+                <Card style={{ textAlign: 'center', backgroundColor: "var(--background)", margin: "10px" }}>
+                    <Card.Header as={"h3"} className="text-center">
+                        Welcome to SQueaL
+                    </Card.Header>
+                    <Card.Body style={{ backgroundColor: "var(--background)" }}>
+                        <ToggleButton
+                            onClick={this.isInstructorClicked}
+                            style={{marginTop: "10px"}}
+                            >
+                            {this.state.isInstructor ? 'Signing in as Instructor' : 'Signing in as Student'}
+                        </ToggleButton>
+                        <SignInButton {...this.props} isInstructor={this.state.isInstructor} ></SignInButton>
+                    </Card.Body>
+                    {/* {this.state.correct ? <p className="confirmation">Correct! "{this.state.optionSelectedString}"</p> : this.state.indexChecked != -1 ? <p className="confirmation">Incorrect</p> : <></>}  */}
+                </Card>
+            </div>
+        )
     }
 }
 
