8a0df6ce336d9018b9c815553a6cfe6dc0a79637
jdm8943 <jdm8943@g.rit.edu>
2024-02-25 00:56:46 -0500

    committing schutff

--- src/components/studentComponents/StudentCoursesListPage/StudentCoursesListPage.jsx
+++ src/components/studentComponents/StudentCoursesListPage/StudentCoursesListPage.jsx
@@ -5,47 +5,56 @@ import { getFirestore, collection, getDocs, query, doc, getDoc, where, setDoc } 
 class StudentCoursesListPage extends Component {
     constructor(props) {
         super(props);
+        console.log("constructor")
         this.state = {
             classCodes: null,
-            courseRefs: null,
-        };
-        
+            coursesData: null,
+        };  
+  
         getDoc(this.props.userRef)
             .then((userSnap) => {
+                console.log("weee")
                 this.setState({
                     classCodes: userSnap.classCodes,
                 }, () => {
                     // callback because must wait for classCodes to be populated
                     this.populateCourseReferences();
+                    console.log(this.state.classCodes)
                 })
-        });
+            });
     }
 
     populateCourseReferences = () => {
-        getDoc(this.props.userRef)
-            .then((userSnap) => {
-                this.setState({
-                    classCodes: userSnap.classCodes,
-                })
+        // Loop through IDs and fetch documents
+        const courseIdArray = this.state.classCodes;
+        const courseDocProms = courseIdArray.map((courseid) => {
+            const courseRef = doc(courseid);
+            return getDoc(courseRef)
+                .then((courseSnapshot) => {
+                    if (courseSnapshot.exists) {
+                        return { ...courseSnapshot.data() };
+                    } else {
+                        // Handle non-existent documents
+                        return null;
+                    }
+                });
         });
 
-        // Loop through IDs and fetch documents
-        const courseIdArray = this.state.courseCodes;
-        const courseDocProms = courseIdArray.map((id) => {
-            const courseRef = doc(id);
-            return courseRef.get().then((docSnap) => {
-                if (docSnap.exists) {
-                    return { ...docSnap.data(), id: docSnap.id }; // Add ID to document object
-                } else {
-                    // Handle non-existent documents
-                    return null;
-                }
+        Promise.all(courseDocProms)
+            .then((fetchedCourses) => {
+                const filteredCourses = fetchedCourses.filter(Boolean); // Filter out null values
+                this.setState({ 
+                    coursesData: filteredCourses 
+                }, 
+                () => {console.log(this.state.coursesData)
+                });
             });
-        });
+
+        
     }
 
     render() {
-        return <div>StudentCoursesListPage</div>
+        return <div>StudentCoursesL istPage cahn ge</div>
     }
 }
 
