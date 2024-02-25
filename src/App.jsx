9d4df0b61673f1f3ab53f04ba9645e487c951df0
jdm8943 <jdm8943@g.rit.edu>
2024-02-24 22:06:31 -0500

    pass more parameters with loginsuccessful

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,43 @@ import './App.css';
 import StudentPage from './components/studentComponents/StudentPage/StudentPage.jsx';
 import InstructorPage from './components/instructorComponents/InstructorPage/InstructorPage.jsx';
 import LoginPage from './components/LoginPage/LoginPage.jsx';
-
+import { getFirestore, collection, getDocs, query } from "firebase/firestore";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      displayName: null,
-      uid: null,
-      isInstructor: null,
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            displayName: null,
+            uid: null,
+            email: null,
+            isInstructor: null,
+            firestoredb: null
+        };
+    }
 
-  loginSuccessful = (displayName, uid, isInstructor) => {
-    this.setState({displayName: displayName, uid: uid, isInstructor: isInstructor})
-  }
+    processUserFirestore = () => {
+        const usersRef = collection(this.state.firestoredb, "Users");
 
-  render(){
-    return ( !this.state.uid ? <LoginPage loginSuccessful={this.loginSuccessful}></LoginPage> : this.state.isInstructor ? <InstructorPage {...this.state}/> : <StudentPage {...this.state}/>
-    );
-  }
+    }
+
+    loginSuccessful = (displayName, uid, isInstructor, database, email) => {
+        this.setState(
+            { 
+                displayName: displayName, 
+                uid: uid,
+                email: email,
+                isInstructor: isInstructor,
+                firestoredb: database, 
+            },
+            () => this.processUserFirestore()
+        );
+
+    }
+
+    render() {
+        return (!this.state.uid ? <LoginPage loginSuccessful={this.loginSuccessful}></LoginPage> : this.state.isInstructor ? <InstructorPage {...this.state} /> : <StudentPage {...this.state} />
+        );
+    }
 }
 
 export default App;
