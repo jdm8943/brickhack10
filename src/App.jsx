0e8e0c9b547de22adfba49519c59e765f5437863
jdm8943 <jdm8943@g.rit.edu>
2024-02-24 22:54:39 -0500

    user sign in creates new user automatically

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css';
 import StudentPage from './components/studentComponents/StudentPage/StudentPage.jsx';
 import InstructorPage from './components/instructorComponents/InstructorPage/InstructorPage.jsx';
 import LoginPage from './components/LoginPage/LoginPage.jsx';
-import { getFirestore, collection, getDocs, query } from "firebase/firestore";
+import { getFirestore, collection, getDocs, query, doc, getDoc, where, setDoc } from "firebase/firestore";
 
 class App extends Component {
     constructor(props) {
@@ -13,23 +13,54 @@ class App extends Component {
             uid: null,
             email: null,
             isInstructor: null,
-            firestoredb: null
+            firestoredb: null,
+            userInfo: null,
         };
     }
 
     processUserFirestore = () => {
         const usersRef = collection(this.state.firestoredb, "Users");
+        const dref = doc(usersRef, this.state.uid)
+        getDoc(dref)
+            .then((docSnapshot) => {
+                // if no user, prompt to create a new user
+                // if user is found, continue onto default landing page
+                if (!docSnapshot.exists()) {
+                    console.log("no user found. create new");
+                    const newUser = {
+                        displayName: this.state.displayName,
+                        email: this.state.email,
+                        uid: this.state.uid,
+                        type: (this.state.isInstructor ? "instructor" : "student"),
+                        classCodes: [],
+                        questionTags: [],
 
+                    }
+                    setDoc(dref, newUser)
+                        .then(() => {
+                            console.log("User written with ID:", dref.id);
+                        })
+                        .catch((error) => {
+                            console.error("Error adding document:", error);
+                        });
+                } else {
+                    const uData = docSnapshot.data();
+                    console.log(uData)
+                }
+            })
+            .catch((error) => {
+                console.error("Error getting document:", error);
+            });
     }
 
     loginSuccessful = (displayName, uid, isInstructor, database, email) => {
         this.setState(
-            { 
-                displayName: displayName, 
+            {
+                displayName: displayName,
                 uid: uid,
                 email: email,
                 isInstructor: isInstructor,
-                firestoredb: database, 
+                firestoredb: database,
             },
             () => this.processUserFirestore()
         );
