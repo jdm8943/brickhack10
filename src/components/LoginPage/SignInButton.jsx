66109de129974813ddf6ec99cb1ffdadcbdce0c0
Zak Kaufman <Zak3148@rit.edu>
2024-02-27 12:40:17 -0500

    removed apikey

--- src/components/LoginPage/SignInButton.jsx
+++ src/components/LoginPage/SignInButton.jsx
@@ -4,20 +4,11 @@ import { getAnalytics } from "@firebase/analytics";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "@firebase/auth";
 import { getFirestore, collection, getDocs, query } from "firebase/firestore";
 import { Button } from 'react-bootstrap'
+import { firebaseConfig } from "./apiKeys";
 
 class SignInButton extends React.Component {
     constructor(props) {
         super(props);
-
-        let firebaseConfig = {
-            apiKey: "AIzaSyDvsrgc09GqGD8y3a3H3G0xEdtx7vxF5Fg",
-            authDomain: "brickhack10.firebaseapp.com",
-            projectId: "brickhack10",
-            storageBucket: "brickhack10.appspot.com",
-            messagingSenderId: "1090128869892",
-            appId: "1:1090128869892:web:cc063de784b73b2842d351",
-            measurementId: "G-4J4WL8FHTK",
-        };
 
         this.state = {
             app: initializeApp(firebaseConfig),
